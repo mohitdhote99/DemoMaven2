747304f72c10236ff75dea894e2a70182f4d0a61
MohitDhote <mohitdhote99@gmail.com>
2022-04-28 15:00:56 +0530

    new branch edit one

--- DemoMaven2/src/main/java/com/lti/practice/NewBranchClass.java
+++ DemoMaven2/src/main/java/com/lti/practice/NewBranchClass.java
@@ -4,7 +4,7 @@ public class NewBranchClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("new brach here hello all");
+		System.out.println("new brach here hello alkkkkl");
 	}
 
 }
