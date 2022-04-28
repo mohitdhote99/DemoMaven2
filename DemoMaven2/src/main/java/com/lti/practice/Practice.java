c1645d860237c1203fecab1423fd3fe31b0c87ab
MohitDhote <mohitdhote99@gmail.com>
2022-04-28 14:17:00 +0530

    CommitOne

---
+++ DemoMaven2/src/main/java/com/lti/practice/Practice.java
@@ -0,0 +1,10 @@
+package com.lti.practice;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mohit you complete second task");
+	}
+
+}
