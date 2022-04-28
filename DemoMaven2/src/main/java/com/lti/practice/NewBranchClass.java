e524825c1c972934cbbf3f291c37f0fd0342f28f
MohitDhote <mohitdhote99@gmail.com>
2022-04-28 15:00:15 +0530

    second new branch added

---
+++ DemoMaven2/src/main/java/com/lti/practice/NewBranchClass.java
@@ -0,0 +1,10 @@
+package com.lti.practice;
+
+public class NewBranchClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new brach here hello all");
+	}
+
+}
